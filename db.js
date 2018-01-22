fd6b741a38185ca24b259eb34439dee27f12a873
kchoo <kchoodev@gmail.com>
2018-01-21 20:10:53 -0600

    Create db.js

---
+++ db.js
@@ -0,0 +1,58 @@
+const {Client} = require('pg');
+
+function KchooDB(config) {
+	this.client = new Client(config);
+
+	this.client.connect();
+}
+
+KchooDB.prototype.addSource = function ({site, id}) {
+	return this.client.
+		query({
+			text: `
+				INSERT INTO
+					sources (site_id, state, remote_identifier)
+				VALUES
+					(
+						(SELECT id FROM sites WHERE name = $1),
+						(SELECT id FROM source_states WHERE name = 'pending'),
+						$2
+					)
+				RETURNING
+					id;
+			`,
+			values: [site, id]
+		}).
+		then(function ({rows: [{id}]}) {
+			return id;
+		});
+};
+
+KchooDB.prototype.getPendingSources = function ({site}) {
+	return this.client.
+		query({
+			text: `
+				UPDATE sources
+				SET state = (SELECT id FROM source_states WHERE name = 'populating')
+				WHERE id IN
+					(
+						SELECT id
+						FROM sources
+						WHERE site_id = (SELECT id FROM sites WHERE name = $2)
+							AND last last_processed_id IS NULL
+						ORDER BY id
+						LIMIT 1
+					)
+				RETURNING id, remote_identifier;
+			`,
+			values: [state, site, count]
+		}).
+		then(function ({rows}) {
+			return rows.
+				map(function ({id: dbID, remote_identifier: twitterID}) {
+					return {dbID, twitterID};
+				});
+		});
+};
+
+module.exports = KchooDB;
